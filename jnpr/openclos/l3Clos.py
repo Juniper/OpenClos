705e7f5a5d023f29f79869db56b7a0b694f4cb69
Moloy <moloyc@juniper.net>
2014-08-11 11:30:25 -0700

    initial commit of openclos code

---
+++ jnpr/openclos/l3Clos.py
@@ -0,0 +1,448 @@
+'''
+Created on May 23, 2014
+
+@author: moloyc
+'''
+
+import yaml
+import os
+import json
+import math
+from netaddr import IPNetwork
+from jnpr.openclos.model import Pod, Device, Interface, InterfaceLogical, InterfaceDefinition, Base
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker, scoped_session, exc
+
+
+configLocation = 'conf/'
+junosTemplateLocation = configLocation + 'junosTemplates/'
+
+def loadConfig(confFile = 'openclos.yaml'):
+    '''
+    Loads global configuration and creates hash 'conf'
+    '''
+    try:
+        confStream = open(configLocation + confFile, 'r')
+        conf = yaml.load(confStream)
+        
+    except (OSError, IOError) as e:
+        print "File error:", e
+    except (yaml.scanner.ScannerError) as e:
+        print "YAML error:", e
+        confStream.close()
+    finally:
+        pass
+    return conf
+
+class Dao:
+    def __init__(self, conf):
+        if conf is not None and conf['dbUrl'] is not None:
+            #Change "echo=True" to troubleshoot ORM issue
+            engine = sqlalchemy.create_engine(conf['dbUrl'], echo=False)  
+            Base.metadata.create_all(engine) 
+            session_factory = sessionmaker(bind=engine)
+            self.Session = scoped_session(session_factory)
+        else:
+            raise ValueError("Missing configuration parameter:'dbUrl'")
+        
+    # Don't remove session after each operation, it detaches the object from ORM,
+    # which disables further operations on the object like lazy load of collection.
+    # When thread dies, it gets GCed automatically
+    def createObjects(self, objects):
+        session = self.Session()
+        try:
+            session.add_all(objects)
+            session.commit()
+        finally:
+            #self.Session.remove()
+            pass
+    
+    def updateObjects(self, objects):
+        session = self.Session()
+        try:
+            for obj in objects:
+                session.merge(obj)
+            session.commit()
+        finally:
+            #self.Session.remove()
+            pass
+    def getAll(self, objectType):
+        session = self.Session()
+        try:
+            return session.query(objectType).order_by(objectType.name).all()
+        finally:
+            #self.Session.remove()
+            pass
+
+    def getUniqueObjectByName(self, objectType, name):
+        session = self.Session()
+        try:
+            return session.query(objectType).filter_by(name = name).one()
+        finally:
+            #self.Session.remove()
+            pass
+
+    def getObjectsByName(self, objectType, name):
+        session = self.Session()
+        try:
+            return session.query(objectType).filter_by(name = name).all()
+        finally:
+            #self.Session.remove()
+            pass
+
+class FileOutputHandler():
+    def __init__(self, conf, pod):
+        if conf.get('outputDir') is not None:
+            self.outputDir = conf['outputDir'] + '/' + pod.name
+        else:
+            self.outputDir = 'out/' + pod.name
+        if not os.path.exists(self.outputDir):
+            os.makedirs(self.outputDir)
+
+    def handle(self, pod, device, config):
+        print "Writing config for device: %s" % (device.name)
+        with open(self.outputDir + "/" + device.name + '.conf', 'w') as f:
+                f.write(config)
+
+class L3ClosMediation():
+    def __init__(self, conf = {}):
+        if any(conf) == False:
+            self.conf = loadConfig()
+        else:
+            self.conf = conf
+        self.dao = Dao(self.conf)
+        
+    def loadClosDefinition(self, closDefination = configLocation + 'closTemplate.yaml'):
+        '''
+        Loads clos definition from yaml file and creates pod object
+        '''
+        try:
+            stream = open(closDefination, 'r')
+            yamlStream = yaml.load(stream)
+            
+            self.createPods(yamlStream['pods'])
+        except (OSError, IOError) as e:
+            print "File error:", e
+        except (yaml.scanner.ScannerError) as e:
+            print "YAML error:", e
+            stream.close()
+        finally:
+            pass
+       
+    def createPods(self, podsDict):
+        pods = []
+        for name, pod in podsDict.iteritems():
+            pods.append(Pod(name, **pod))
+        self.dao.createObjects(pods)
+
+    def processTopology(self, podName):
+        '''
+        Finds Pod object by name and process topology
+        It also creates the output folders for pod
+        '''
+        try:
+            pod = self.dao.getUniqueObjectByName(Pod, podName)
+        except (exc.NoResultFound) as e:
+            raise ValueError("No Pod found with pod name: '%s', exc.NoResultFound: %s" % (podName, e.message))
+        except (exc.MultipleResultsFound) as e:
+            raise ValueError("Multiple Pods found with pod name: '%s', exc.MultipleResultsFound: %s" % (podName, e.message))
+ 
+        if pod.topology is not None:
+            json_data = open(configLocation + pod.topology)
+            data = json.load(json_data)
+            json_data.close()    
+            
+            self.createSpineIFDs(pod, data['spines'])
+            self.createLeafIFDs(pod, data['leafs'])
+            self.createLinkBetweenIFDs(pod, data['links'])
+            self.allocateResource(pod)
+            self.output = FileOutputHandler(self.conf, pod)
+            self.generateConfig(pod)
+
+        else:
+            raise ValueError("No topology found for pod name: '%s'", (podName))
+
+    def createSpineIFDs(self, pod, spines):
+        devices = []
+        interfaces = []
+        for spine in spines:
+            device = Device(spine['name'], spine['user'], spine['password'], 'spine', spine['mgmt_ip'], pod)
+            devices.append(device)
+            # all downlink IFDs towards leaf
+            # TODO: range value and idfname should come from property file
+            for num in range(0, 24):
+                ifd = InterfaceDefinition("et-0/0/" + str(num), device)
+                interfaces.append(ifd)
+        self.dao.createObjects(devices)
+        self.dao.createObjects(interfaces)
+
+    def createLeafIFDs(self, pod, leafs):
+        devices = []
+        interfaces = []
+        for leaf in leafs:
+            device = Device(leaf['name'], leaf['user'], leaf['password'], 'leaf', leaf['mgmt_ip'], pod)
+            devices.append(device)
+            # TODO: range value and idfname should come from property file
+            # all uplink IFDs towards spine
+            for num in range(48, 54):
+                ifd = InterfaceDefinition("et-0/0/" + str(num), device)
+                interfaces.append(ifd)
+            # all downlink IFDs towards server
+            for num in range(0, 48):
+                ifd = InterfaceDefinition("xe-0/0/" + str(num), device)
+                interfaces.append(ifd)
+        self.dao.createObjects(devices)
+        self.dao.createObjects(interfaces)
+
+    def createLinkBetweenIFDs(self, pod, links):
+        # Caching all interfaces by deviceName...interfaceName for easier lookup
+        interfaces = {}
+        modifiedObjects = []
+        for device in pod.devices:
+            for interface in device.interfaces:
+                name = device.name + '...' + interface.name
+                interfaces[name] = interface
+
+        for link in links:
+            spineIntf = interfaces[link['s_name'] + '...' + link['s_port']]
+            leafIntf = interfaces[link['l_name'] + '...' + link['l_port']]
+            # hack to add relation from both sides as on ORM it is oneway one-to-one relation
+            spineIntf.peer = leafIntf
+            leafIntf.peer = spineIntf
+            modifiedObjects.append(spineIntf)
+            modifiedObjects.append(leafIntf)
+        self.dao.updateObjects(modifiedObjects)
+        
+    def getLeafSpineFromPod(self, pod):
+        '''
+        utility method to get list of spines and leafs of a pod
+        returns dict with list for 'spines' and 'leafs'
+        '''
+        deviceDict = {}
+        deviceDict['leafs'] = []
+        deviceDict['spines'] = []
+        for device in pod.devices:
+            if (device.role == 'leaf'):
+                deviceDict['leafs'].append(device)
+            elif (device.role == 'spine'):
+                deviceDict['spines'].append(device)
+        return deviceDict
+    
+    def allocateResource(self, pod):
+        self.allocateLoopback(pod, pod.loopbackPrefix, pod.devices)
+        leafSpineDict = self.getLeafSpineFromPod(pod)
+        self.allocateIrb(pod, pod.vlanPrefix, leafSpineDict['leafs'])
+        self.allocateInterconnect(pod.interConnectPrefix, leafSpineDict['spines'], leafSpineDict['leafs'])
+        self.allocateAsNumber(pod.spineAS, pod.leafAS, leafSpineDict['spines'], leafSpineDict['leafs'])
+        
+    def allocateLoopback(self, pod, loopbackPrefix, devices):
+        numOfIps = len(devices) + 2 # +2 for network and broadcast
+        numOfBits = int(math.ceil(math.log(numOfIps, 2))) 
+        cidr = 32 - numOfBits
+        lo0Block = IPNetwork(loopbackPrefix + "/" + str(cidr))
+        lo0Ips = list(lo0Block.iter_hosts())
+        
+        interfaces = []
+        pod.allocatedLoopbackBlock = str(lo0Block.cidr)
+        for device in devices:
+            ifl = InterfaceLogical('lo0.0', device, str(lo0Ips.pop(0)) + '/32')
+            interfaces.append(ifl)
+        self.dao.createObjects(interfaces)
+
+    def allocateIrb(self, pod, irbPrefix, leafs):
+        numOfHostIpsPerSwitch = 254     #TODO: should come from property file
+        numOfSubnets = len(leafs)
+        bitsPerSubnet = int(math.ceil(math.log(numOfHostIpsPerSwitch + 2, 2)))  # +2 for network and broadcast
+        cidrForEachSubnet = 32 - bitsPerSubnet
+
+        numOfIps = (numOfSubnets * (numOfHostIpsPerSwitch + 2)) # +2 for network and broadcast
+        numOfBits = int(math.ceil(math.log(numOfIps, 2))) 
+        cidr = 32 - numOfBits
+        irbBlock = IPNetwork(irbPrefix + "/" + str(cidr))
+        irbSubnets = list(irbBlock.subnet(cidrForEachSubnet))
+        
+        interfaces = [] 
+        pod.allocatedIrbBlock = str(irbBlock.cidr)
+        for leaf in leafs:
+            ipAddress = list(irbSubnets.pop(0).iter_hosts())[0]
+            # TODO: would be better to get irb.1 from property file as .1 is VLAN ID
+            ifl = InterfaceLogical('irb.1', leaf, str(ipAddress) + '/' + str(cidrForEachSubnet)) 
+            interfaces.append(ifl)
+        self.dao.createObjects(interfaces)
+
+    def allocateInterconnect(self, interConnectPrefix, spines, leafs):
+        numOfIpsPerInterconnect = 2
+        numOfSubnets = len(spines) * len(leafs)
+        # no need to add +2 for network and broadcast, as junos supports /31
+        # TODO: it should be configurable and come from property file
+        bitsPerSubnet = int(math.ceil(math.log(numOfIpsPerInterconnect, 2)))    # value is 1  
+        cidrForEachSubnet = 32 - bitsPerSubnet  # value is 31 as junos supports /31
+
+        numOfIps = (numOfSubnets * (numOfIpsPerInterconnect)) # no need to add +2 for network and broadcast
+        numOfBits = int(math.ceil(math.log(numOfIps, 2))) 
+        cidr = 32 - numOfBits
+        interconnectBlock = IPNetwork(interConnectPrefix + "/" + str(cidr))
+        interconnectSubnets = list(interconnectBlock.subnet(cidrForEachSubnet))
+
+        interfaces = [] 
+        for spine in spines:
+            ifdsHasPeer = self.dao.Session().query(InterfaceDefinition).filter(InterfaceDefinition.device_id == spine.id).filter(InterfaceDefinition.peer != None).order_by(InterfaceDefinition.name).all()
+            for spineIfdHasPeer in ifdsHasPeer:
+                subnet =  interconnectSubnets.pop(0)
+                ips = list(subnet)
+                
+                spineEndIfl= InterfaceLogical(spineIfdHasPeer.name + '.0', spine, str(ips.pop(0)) + '/' + str(cidrForEachSubnet))
+                spineIfdHasPeer.layerAboves.append(spineEndIfl)
+                interfaces.append(spineEndIfl)
+                
+                leafEndIfd = spineIfdHasPeer.peer
+                leafEndIfl= InterfaceLogical(leafEndIfd.name + '.0', leafEndIfd.device, str(ips.pop(0)) + '/' + str(cidrForEachSubnet))
+                leafEndIfd.layerAboves.append(leafEndIfl)
+                interfaces.append(leafEndIfl)
+        self.dao.createObjects(interfaces)
+
+    def allocateAsNumber(self, spineAsn, leafAsn, spines, leafs):
+        devices = []
+        for spine in spines:
+            spine.asn = spineAsn
+            spineAsn += 1
+            devices.append(spine)
+        for leaf in leafs:
+            leaf.asn = leafAsn
+            leafAsn += 1
+            devices.append(leaf)
+        self.dao.updateObjects(devices)
+
+    def generateConfig(self, pod):
+        for device in pod.devices:
+            config = self.createBaseConfig(device)
+            config += self.createInterfaces(device)
+            config += self.createRoutingOption(device)
+            config += self.createProtocols(device)
+            config += self.createPolicyOption(device)
+            config += self.createVlan(device)
+            self.output.handle(pod, device, config)
+            
+    def createBaseConfig(self, device):
+        with open(junosTemplateLocation + 'baseTemplate.txt', 'r') as f:
+            baseTemplate = f.read()
+            f.close()
+            return baseTemplate
+
+    def createInterfaces(self, device): 
+        with open(junosTemplateLocation + 'interface_stanza.txt', 'r') as f:
+            interfaceStanza = f.read()
+            f.close()
+        
+        with open(junosTemplateLocation + 'lo0_stanza.txt', 'r') as f:
+            lo0Stanza = f.read()
+            f.close()
+            
+        with open(junosTemplateLocation + 'mgmt_interface.txt', 'r') as f:
+            mgmtStanza = f.read()
+            f.close()
+
+        with open(junosTemplateLocation + 'rvi_stanza.txt', 'r') as f:
+            rviStanza = f.read()
+            f.close()
+            
+        with open(junosTemplateLocation + 'server_interface_stanza.txt', 'r') as f:
+            serverInterfaceStanza = f.read()
+            f.close()
+            
+        config = "interfaces {" + "\n" 
+        # management interface
+        candidate = mgmtStanza.replace("<<<mgmt_address>>>", device.managementIp)
+        config += candidate
+                
+        #loopback interface
+        loopbackIfl = self.dao.Session.query(InterfaceLogical).join(Device).filter(InterfaceLogical.name == 'lo0.0').filter(Device.id == device.id).one()
+        candidate = lo0Stanza.replace("<<<address>>>", loopbackIfl.ipaddress)
+        config += candidate
+
+        # For Leaf add IRB and server facing interfaces        
+        if device.role == 'leaf':
+            irbIfl = self.dao.Session.query(InterfaceLogical).join(Device).filter(InterfaceLogical.name == 'irb.1').filter(Device.id == device.id).one()
+            candidate = rviStanza.replace("<<<address>>>", irbIfl.ipaddress)
+            config += candidate
+            config += serverInterfaceStanza
+
+        # Interconnect interfaces
+        deviceInterconnectIfds = self.dao.Session.query(InterfaceDefinition).join(Device).filter(InterfaceDefinition.peer != None).filter(Device.id == device.id).order_by(InterfaceDefinition.name).all()
+        for interconnectIfd in deviceInterconnectIfds:
+            peerDevice = interconnectIfd.peer.device
+            interconnectIfl = interconnectIfd.layerAboves[0]
+            namePlusUnit = interconnectIfl.name.split('.')  # example et-0/0/0.0
+            candidate = interfaceStanza.replace("<<<ifd_name>>>", namePlusUnit[0])
+            candidate = candidate.replace("<<<unit>>>", namePlusUnit[1])
+            candidate = candidate.replace("<<<description>>>", "facing_" + peerDevice.name)
+            candidate = candidate.replace("<<<address>>>", interconnectIfl.ipaddress)
+            config += candidate
+                
+        config += "}\n"
+        return config
+
+    def createRoutingOption(self, device):
+        with open(junosTemplateLocation + 'routing_options_stanza.txt', 'r') as f:
+            routingOptionStanza = f.read()
+
+        loopbackIfl = self.dao.Session.query(InterfaceLogical).join(Device).filter(InterfaceLogical.name == 'lo0.0').filter(Device.id == device.id).one()
+        loopbackIpWithNoCidr = loopbackIfl.ipaddress.split('/')[0]
+        
+        candidate = routingOptionStanza.replace("<<<routerId>>>", loopbackIpWithNoCidr)
+        candidate = candidate.replace("<<<asn>>>", str(device.asn))
+        
+        return candidate
+
+    def createProtocols(self, device):
+        with open(junosTemplateLocation + 'protocols_stanza_init.txt', 'r') as f:
+            config = f.read()
+
+        with open(junosTemplateLocation + 'protocols_stanza_neighbor.txt', 'r') as f:
+            neighborStanza = f.read()
+            
+        deviceInterconnectIfds = self.dao.Session.query(InterfaceDefinition).join(Device).filter(InterfaceDefinition.peer != None).filter(Device.id == device.id).order_by(InterfaceDefinition.name).all()
+        for ifd in deviceInterconnectIfds:
+            peerIfd = ifd.peer
+            peerDevice = peerIfd.device
+            peerInterconnectIfl = peerIfd.layerAboves[0]
+            peerInterconnectIpNoCidr = peerInterconnectIfl.ipaddress.split('/')[0]
+            candidate = neighborStanza.replace("<<<peer_ip>>>", peerInterconnectIpNoCidr)
+            candidate = candidate.replace("<<<peer_asn>>>", str(peerDevice.asn))
+            config += candidate
+            
+        with open(junosTemplateLocation + 'protocols_stanza_end.txt', 'r') as f:
+            config += f.read()
+
+        return config        
+            
+    def createPolicyOption(self, device):
+        pod = device.pod
+        
+        with open(junosTemplateLocation + 'policy_options_stanza.txt', 'r') as f:
+            config = f.read()
+        
+        config = config.replace("<<<lo0_in_subnet>>>", pod.allocatedLoopbackBlock)
+        config = config.replace("<<<irb_in_subnet>>>", pod.allocatedIrbBlock)
+
+        if device.role == 'leaf':
+            deviceLoopbackIfl = self.dao.Session.query(InterfaceLogical).join(Device).filter(InterfaceLogical.name == 'lo0.0').filter(Device.id == device.id).one()
+            deviceIrbIfl = self.dao.Session.query(InterfaceLogical).join(Device).filter(InterfaceLogical.name == 'irb.1').filter(Device.id == device.id).one()
+            config = config.replace("<<<lo0_out_subnet>>>", deviceLoopbackIfl.ipaddress)
+            config = config.replace("<<<irb_out_subnet>>>", deviceIrbIfl.ipaddress)
+        else:
+            config = config.replace("<<<lo0_out_subnet>>>", pod.allocatedLoopbackBlock)
+            config = config.replace("<<<irb_out_subnet>>>", pod.allocatedIrbBlock)
+            
+        return config
+
+    def createVlan(self, device):
+        with open(junosTemplateLocation + 'vlans_stanza.txt', 'r') as f:
+            config = f.read()
+        return config
+        
+if __name__ == '__main__':
+    l3ClosMediation = L3ClosMediation()
+    l3ClosMediation.loadClosDefinition()
+    l3ClosMediation.processTopology('labLeafSpine')
+
---
+++ jnpr/openclos/model.py
@@ -0,0 +1,179 @@
+'''
+Created on Jul 8, 2014
+
+@author: moloyc
+
+'''
+import uuid
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
+Base = declarative_base()
+
+class ManagedElement(object):
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+    def __str__(self):
+        return str(self.__dict__)
+    def __repr__(self):
+        return self.__str__()
+    @staticmethod
+    def validateEnum(enumName, value, enumList):
+        # Validate enumerated value, a restriction on string.
+        error = False
+        if isinstance(value, list):
+            error = set(value) - set(enumList)
+        else:
+            error = value not in enumList
+        if error:
+            raise ValueError("%s('%s') must be one of %s" % (enumName, value, enumList))
+    
+class Pod(ManagedElement, Base):
+    __tablename__ = 'pod'
+    id = Column(String(60), primary_key=True)
+    name = Column(String(100))
+    spineCount = Column(Integer)
+    spineDeviceType = Column(String(100))
+    leafCount = Column(Integer)
+    leafDeviceType = Column(String(100))
+    interConnectPrefix = Column(String(32))
+    vlanPrefix = Column(String(32))
+    loopbackPrefix = Column(String(32))
+    spineAS = Column(Integer)
+    leafAS = Column(Integer)
+    topologyType = Column(String(100))
+    topology = Column(String(100))
+    allocatedInterConnectBlock = Column(String(32))
+    allocatedIrbBlock = Column(String(32))
+    allocatedLoopbackBlock = Column(String(32))
+
+    TopologyTypeEnum = ['3Stage', '5Stage', 'Pod']
+  
+    def __init__(self, name, **kwargs):
+        '''
+        Creates a Pod object from dict, if following fields are missing, it throws ValueError
+        interConnectPrefix, vlanPrefix, loopbackPrefix, spineAS, leafAS
+        '''
+        self.id = str(uuid.uuid4())
+        self.name = name
+                   
+        error = ''
+        self.spineCount = kwargs.get('spineCount')
+        if self.spineCount is None:
+            error += 'spineCount, '
+        self.spineDeviceType = kwargs.get('spineDeviceType')
+        if self.spineDeviceType is None:
+            error += 'spineDeviceType, '
+        self.leafCount = kwargs.get('leafCount')
+        if self.leafCount is None:
+            error += 'leafCount, '
+        self.leafDeviceType = kwargs.get('spineDeviceType')
+        if self.leafDeviceType is None:
+            error += 'leafDeviceType, '
+        self.interConnectPrefix = kwargs.get('interConnectPrefix')
+        if self.interConnectPrefix is None:
+            error += 'interConnectPrefix, '
+        self.vlanPrefix = kwargs.get('vlanPrefix')
+        if self.vlanPrefix is None:
+            error += 'vlanPrefix, '
+        self.loopbackPrefix = kwargs.get('loopbackPrefix')
+        if self.loopbackPrefix is None:
+            error += 'loopbackPrefix, '
+        if kwargs.has_key('spineAS'):
+            self.spineAS = int(kwargs.get('spineAS'))
+        else:
+            error += 'spineAS, '
+        if kwargs.has_key('leafAS'):
+            self.leafAS = int(kwargs.get('leafAS'))
+        else:
+            error += 'leafAS, '
+        self.topologyType = kwargs.get('topologyType')
+        if self.topologyType is None:
+            error += 'topologyType, '
+        self.topology = kwargs.get('topology')
+        
+        if error != '':
+            raise ValueError('Missing required fields: ' + error[:-2])
+        super(Pod, self).__init__(**kwargs)
+        
+class Device(ManagedElement, Base):
+    __tablename__ = 'device'
+    id = Column(String(60), primary_key=True)
+    name = Column(String(100))
+    username = Column(String(100))
+    pwd = Column(String(100))
+    role = Column(String(32))
+    managementIp = Column(String(32))
+    asn = Column(Integer)
+    pod_id = Column(Integer, ForeignKey('pod.id'), nullable = False)
+    pod = relationship("Pod", backref=backref('devices', order_by=name))
+        
+    def __init__(self, name, username, pwd, role, mgmtIp, pod):
+        '''
+        Creates Device object.
+        '''
+        self.id = str(uuid.uuid4())
+        self.name = name
+        self.username = username
+        self.pwd = pwd
+        self.role = role
+        self.managementIp = mgmtIp
+        self.pod = pod
+     
+               
+class Interface(Base):
+    __tablename__ = 'interface'
+    id = Column(String(60), primary_key=True)
+    name = Column(String(100))
+    type = Column(String(100))
+    device_id = Column(Integer, ForeignKey('device.id'), nullable = False)
+    device = relationship("Device",backref=backref('interfaces', order_by=name))
+    peer_id = Column(Integer, ForeignKey('interface.id'))
+    peer = relationship('Interface', foreign_keys=[peer_id], uselist=False, post_update=True, )
+    layer_below_id = Column(Integer, ForeignKey('interface.id'))
+    layerAboves = relationship('Interface', foreign_keys=[layer_below_id])
+
+    __mapper_args__ = {
+        'polymorphic_identity':'interface',
+        'polymorphic_on':type
+    }
+        
+    def __init__(self, name, device):
+        self.id = str(uuid.uuid4())
+        self.name = name
+        self.device = device
+        
+class InterfaceLogical(Interface):
+    __tablename__ = 'IFL'
+    id = Column(String(60), ForeignKey('interface.id' ), primary_key=True)
+    ipaddress = Column(String(40))
+    mtu = Column(Integer)
+    
+    __mapper_args__ = {
+        'polymorphic_identity':'logical',
+    }
+    
+    def __init__(self, name, device, ipaddress=None, mtu=0):
+        '''
+        Creates Logical Interface object.
+        ipaddress is optional so that it can be allocated later
+        mtu is optional, default value is taken from global setting
+        '''
+        super(InterfaceLogical, self).__init__(name, device)
+        self.ipaddress = ipaddress
+        self.mtu = mtu
+
+class InterfaceDefinition(Interface):
+    __tablename__ = 'IFD'
+    id = Column(String(60), ForeignKey('interface.id' ), primary_key=True)
+    mtu = Column(Integer)
+        
+    __mapper_args__ = {
+        'polymorphic_identity':'physical',
+    }
+
+    def __init__(self, name, device,  mtu=0):
+        
+        super(InterfaceDefinition, self).__init__(name, device)
+        self.mtu = mtu
+        
